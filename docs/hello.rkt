fa69594902ae6b26571ba81cdf955329e07b5f91
Kristopher Micinski <krismicinski@gmail.com>
2021-02-08 22:43:05 -0500

    webpage update

---
+++ docs/hello.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+;; Exercise 0: complete two simple functions
+(provide implies-value
+         point-distance)
+
+; Compute the truth value of the proposition "x --> y" where x and y are booleans
+(define (implies-value x y)
+  'todo)
+
+; Compute the distance between two (x,y) pairs of integers
+(define (point-distance x0 y0 x1 y1)
+  'todo)
